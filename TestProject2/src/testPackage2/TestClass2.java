9a2bd1b2e786ffafa3588b3a487f45a5088ef05e
SeemaPatil2509 <126742816+SeemaPatil2509@users.noreply.github.com>
2023-03-05 20:59:20 +0530

    SauceLogin commit

---
+++ TestProject2/src/testPackage2/TestClass2.java
@@ -0,0 +1,12 @@
+package testPackage2;
+
+public class TestClass2 {
+	
+	public static void main(String args[])
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.sauceDemo.com/");
+		
+		
+	}
+}
